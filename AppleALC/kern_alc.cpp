def00500704ca2907103a57ce8ecafa545a1e2d9
Taeyon Kim <hi.taeyon@gmail.com>
2019-10-18 11:15:00 -0400

    Add a support for disabling digital audio via boot arg, -alc-disable-digital-audio

--- AppleALC/kern_alc.cpp
+++ AppleALC/kern_alc.cpp
@@ -59,7 +59,7 @@ void AlcEnabler::updateProperties() {
 
 		// Respect desire to disable digital audio. This may be particularly useful for configurations
 		// with broken digital audio, resulting in kernel panics. Ref: https://github.com/acidanthera/bugtracker/issues/513
-		if (hasBuiltinDigitalAudio && devInfo->audioBuiltinAnalog && devInfo->audioBuiltinAnalog->getProperty("No-hda-gfx"))
+		if (hasBuiltinDigitalAudio && devInfo->audioBuiltinAnalog && checkKernelArgument("-alc-disable-digital-audio"))
 			hasBuiltinDigitalAudio = false;
 
 		// Firstly, update Haswell or Broadwell HDAU device for built-in digital audio.
@@ -89,7 +89,7 @@ void AlcEnabler::updateProperties() {
 		// Secondly, update HDEF device and make it support digital audio
 		if (devInfo->audioBuiltinAnalog && validateInjection(devInfo->audioBuiltinAnalog)) {
 			const char *hdaGfx = nullptr;
-			if (hasBuiltinDigitalAudio && !devInfo->audioBuiltinDigital)
+			if (!devInfo->audioBuiltinDigital || !hasBuiltinDigitalAudio)
 				hdaGfx = "onboard-1";
 			updateDeviceProperties(devInfo->audioBuiltinAnalog, devInfo, hdaGfx, true);
 		}
